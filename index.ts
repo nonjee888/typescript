2b7a4f12340baf914e047bfefa7edad583558a4e
nonjee888 <nonjee888@gmail.com>
2022-12-18 11:54:09 +0900

    4강

--- index.ts
+++ index.ts
@@ -132,3 +132,63 @@ let 학교: {
 };
 학교.score[4] = false;
 학교.friend = ["Lee", 학교.teacher];
+
+function 함수1(x: number): number {
+  return x * 2;
+}
+// 함수는 타입 지정 2곳에서 가능 (파라미터, return)
+// 파라미터의 타입지정은 파라미터 옆에 적고 return 우측의 값을 위한 타입지정은 함수명()옆에 적기
+// 파라미터에 타입지정하면 필수 파라미터가 된다
+
+function 함수2(x: number): void {
+  1 + 1;
+}
+// return할 자료가 없는 함수의 타입으로 사용할때 void씀
+// 이 함수에서 return 쓰면 에러 남
+
+function 함수3(x: number | string): void {
+  console.log(x + 1);
+}
+// 파라미터가 옵션일 경우엔 파라미터변수?: 타입
+// 변수?: number는 변수: number | undefined와 같음
+// console.log(x + 1)에서 에러가 나는 이유는 x가 number 타입, string 타입이 아닌 union타입이기 때문
+
+/* 숙제 */
+
+// 1) 이름을 파라미터로 입력하면 콘솔창에 "안녕하세요 홍길동"을 출력해주고
+// 아무것도 파라미터로 입력하지 않고 함수를 사용하면 "이름이 없습니다" 를 출력하는 함수를 만들어봅시다.
+// 파라미터와 return 타입지정도 잘 해봅시다.
+function 함수4(이름?: string): void {
+  if (이름) {
+    console.log("안녕하세요 홍길동");
+  } else {
+    console.log("이름을 입력하세여");
+  }
+}
+
+// 2) 함수에 숫자 또는 문자를 집어넣으면 자릿수를 세어 출력해주는 함수를 만들어보십시오.
+// 예를 들어 '245' 이런 문자를 입력하면 3이 return 되어야합니다.
+// 숫자도 마찬가지로 9567 이런 숫자를 입력하면 4가 return 되어야합니다.
+// 숫자 또는 문자 이외의 자료가 들어오면 안됩니다.
+function 함수5(x: number | string): number {
+  return x.toString().length;
+}
+
+// 3) 결혼 가능 확률을 알려주는 함수를 만들어봅시다.
+// 1. 함수의 파라미터로 월소득(만원단위), 집보유여부(true/false), 매력점수 ('상' or '중' or '하') 를 입력할 수 있어야합니다.
+// 2. 월소득은 만원 당 1점, 집보유시 500점 & 미보유시 0점, 매력점수는 '상'일 때만 100점으로 계산합니다.
+// 3. 총 점수가 600점 이상일 경우 "결혼가능"을 return 해줘야합니다. 그 외엔 아무것도 return하지 않습니다.
+function 함수6(월소득: number, 집보유: boolean, 매력: string): string | void {
+  let score: number = 0;
+  score += 월소득;
+  if (집보유 == true) {
+    score += 500;
+  }
+  if (매력 == "상") {
+    score += 100;
+  }
+  if (score >= 600) {
+    return "결혼가능";
+  }
+}
+console.log(함수6(600, true, "상"));
