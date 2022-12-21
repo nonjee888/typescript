61ee8e25d350354f07402e17f016f476ae962d45
nonjee888 <nonjee888@gmail.com>
2022-12-21 13:30:30 +0900

    강의 5

--- index.js
+++ index.js
@@ -139,3 +139,87 @@ function 함수6(월소득, 집보유, 매력) {
     }
 }
 console.log(함수6(600, true, "상"));
+function 함수7(x) {
+    // union type일 경우 Type Narrowing써야 함
+    // type narrowing이란 if문 등으로 타입을 하나로 정해주는 것
+    if (typeof x === "string") {
+        return x + 1;
+    }
+    else if (typeof x === "number") {
+        return x - 1;
+    }
+    else {
+        return 0;
+    }
+}
+함수7(123);
+function 함수8(x) {
+    var array = [];
+    if (typeof x === "number") {
+        array[0] = x;
+    }
+    else {
+        // if는 else{} 까지 써줘야 에러로부터 안전
+    }
+}
+함수8(123);
+/* Narrowing으로 판정해주는 문법들 */
+// typeof
+// in : 어떤 속성이 object에 있는지
+// instanceof  : 이 오브젝트가 어떤 부모의 자식인지
+function 함수9(x) {
+    var array = [];
+    array[0] = x;
+}
+함수9(123);
+// assertion문법
+// as 문법의 용도
+// 1. Union type을 하나로 확정하고 싶을 때 (타입을 a를 b로 바꾸는건 X)
+// 2. 무슨 타입이 들어올지 100% 확신할 때 쓰기 왜냐면 함수9에 문자가 들어와도 캐치하지 못함.
+// 3. 그래서 남이 짠 코드 수정할 때, 왜 타입에러가 나는지 모르겠을 때...등 비상용으로 자주 쓴다
+// 4. 옛날 as문법은
+var 이름표 = 124;
+이름 + 1; // 현재문법
+이름 + 1; // 옛날문법
+function 변환기(data) {
+    return JSON.parse(data);
+}
+var jake = 변환기('{"name":"Kim"}');
+/* 숙제 */
+// 숙제1. 숫자여러개를 array 자료에 저장해놨는데
+// 가끔 '4', '5' 이런 식의 문자타입의 숫자가 발견되고 있습니다.
+// 이걸 클리닝해주는 함수가 필요합니다.
+// 클리닝함수( ['1', 2, '3'] ) 이렇게 숫자와 문자가 섞인 array를 입력하면
+// [1,2,3] 이렇게 숫자로 깔끔하게 변환되어 나오는 클리닝함수를 만들어오고 타입지정까지 확실히 해보십시오.
+function cleaner(x) {
+    var array = x.map(function (i) { return Number(i); });
+    console.log(array);
+}
+cleaner([1, "2", 3]);
+// 숙제2. 다음과 같은 함수를 만들어보십시오.
+var 철수쌤 = { subject: "math" };
+var 영희쌤 = { subject: ["science", "english"] };
+var 민수쌤 = { subject: ["science", "art", "korean"] };
+// 지금 여러 변수에 선생님이 가르치고 있는 과목이 저장이 되어있습니다.
+// 과목 1개만 가르치는 쌤들은 문자 하나로 과목이 저장이 되어있고
+// 과목 2개 이상 가르치는 쌤들은 array 자료로 과목들이 저장되어있습니다.
+// 철수쌤같은 선생님 object 자료를 집어넣으면
+// 그 선생님이 가르치고 있는 과목중 맨 뒤의 1개를 return 해주는 함수를 만들어봅시다.
+// 그리고 타입지정도 엄격하게 해보도록 합시다.
+// (동작예시)
+// 만들함수( { subject : 'math' } )  //이 경우 'math'를 return
+// 만들함수( { subject : ['science', 'art', 'korean'] } ) //이 경우 'korean'을 return
+// 만들함수( { hello : 'hi' } )  //이 경우 타입에러 나면 됩니다
+function subject(x) {
+    if (Array.isArray(x.subject) === true) {
+        console.log(x.subject[x.subject.length - 1]);
+        return x.subject[x.subject.length - 1];
+    }
+    else {
+        console.log(x.subject);
+        return x.subject;
+    }
+}
+subject({ subject: ["math", "science"] });
+subject({ subject: "math" });
+console.log(subject({ hello: "hi" }));
--- index.ts
+++ index.ts
@@ -192,3 +192,104 @@ function 함수6(월소득: number, 집보유: boolean, 매력: string): string 
   }
 }
 console.log(함수6(600, true, "상"));
+
+function 함수7(x: number | string) {
+  // union type일 경우 Type Narrowing써야 함
+  // type narrowing이란 if문 등으로 타입을 하나로 정해주는 것
+
+  if (typeof x === "string") {
+    return x + 1;
+  } else if (typeof x === "number") {
+    return x - 1;
+  } else {
+    return 0;
+  }
+}
+함수7(123);
+
+function 함수8(x: number | string) {
+  let array: number[] = [];
+  if (typeof x === "number") {
+    array[0] = x;
+  } else {
+    // if는 else{} 까지 써줘야 에러로부터 안전
+  }
+}
+함수8(123);
+/* Narrowing으로 판정해주는 문법들 */
+// typeof
+// in : 어떤 속성이 object에 있는지
+// instanceof  : 이 오브젝트가 어떤 부모의 자식인지
+
+function 함수9(x: number | string) {
+  let array: number[] = [];
+  array[0] = x as number;
+}
+
+함수9(123);
+// assertion문법
+// as 문법의 용도
+// 1. Union type을 하나로 확정하고 싶을 때 (타입을 a를 b로 바꾸는건 X)
+// 2. 무슨 타입이 들어올지 100% 확신할 때 쓰기 왜냐면 함수9에 문자가 들어와도 캐치하지 못함.
+// 3. 그래서 남이 짠 코드 수정할 때, 왜 타입에러가 나는지 모르겠을 때...등 비상용으로 자주 쓴다
+// 4. 옛날 as문법은
+
+let 이름표: number = 124;
+
+(이름 as string) + 1; // 현재문법
+<string>이름 + 1; // 옛날문법
+
+// 이렇게 쓰였었지만 html과 js가 함께 쓰이는 리액트에서는 html태그와 헷갈리기 땜시 as키워드를 주로 씀
+// 5. 이럴때 유용하게 쓰임
+
+type Person = {
+  name: string;
+};
+function 변환기<T>(data: string): T {
+  return JSON.parse(data) as T;
+}
+const jake = 변환기<Person>('{"name":"Kim"}');
+
+/* 숙제 */
+
+// 숙제1. 숫자여러개를 array 자료에 저장해놨는데
+// 가끔 '4', '5' 이런 식의 문자타입의 숫자가 발견되고 있습니다.
+// 이걸 클리닝해주는 함수가 필요합니다.
+// 클리닝함수( ['1', 2, '3'] ) 이렇게 숫자와 문자가 섞인 array를 입력하면
+// [1,2,3] 이렇게 숫자로 깔끔하게 변환되어 나오는 클리닝함수를 만들어오고 타입지정까지 확실히 해보십시오.
+
+function cleaner(x: (number | string)[]) {
+  const array = x.map((i: number | string) => Number(i));
+  console.log(array);
+}
+cleaner([1, "2", 3]);
+
+// 숙제2. 다음과 같은 함수를 만들어보십시오.
+let 철수쌤 = { subject: "math" };
+let 영희쌤 = { subject: ["science", "english"] };
+let 민수쌤 = { subject: ["science", "art", "korean"] };
+// 지금 여러 변수에 선생님이 가르치고 있는 과목이 저장이 되어있습니다.
+// 과목 1개만 가르치는 쌤들은 문자 하나로 과목이 저장이 되어있고
+// 과목 2개 이상 가르치는 쌤들은 array 자료로 과목들이 저장되어있습니다.
+
+// 철수쌤같은 선생님 object 자료를 집어넣으면
+// 그 선생님이 가르치고 있는 과목중 맨 뒤의 1개를 return 해주는 함수를 만들어봅시다.
+// 그리고 타입지정도 엄격하게 해보도록 합시다.
+
+// (동작예시)
+// 만들함수( { subject : 'math' } )  //이 경우 'math'를 return
+// 만들함수( { subject : ['science', 'art', 'korean'] } ) //이 경우 'korean'을 return
+// 만들함수( { hello : 'hi' } )  //이 경우 타입에러 나면 됩니다
+
+function subject(x: { subject: string | string[] }) {
+  if (Array.isArray(x.subject) === true) {
+    console.log(x.subject[x.subject.length - 1]);
+    return x.subject[x.subject.length - 1];
+  } else {
+    console.log(x.subject);
+    return x.subject;
+  }
+}
+subject({ subject: ["math", "science"] });
+subject({ subject: "math" });
+console.log(subject({ hello: "hi" }));
